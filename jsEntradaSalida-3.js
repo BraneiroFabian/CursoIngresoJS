01e0d32e6568e979b282a16fb5c3bd19a95ea7c0
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-06 16:10:46 -0300

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+
+
+    
+    var nombre= document.getElementById("elNombre").value;//La variable toma el valor por un id que esta en el html 
+	alert(nombre);
+
+	
+}
+
+

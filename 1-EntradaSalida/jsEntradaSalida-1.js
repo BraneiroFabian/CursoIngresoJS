733ac49d3b87950689ded1563a40390a03ecc57b
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-06 11:12:47 -0300

    Ejercicio 1
    Aprendi bocha

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,17 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+    var primerPrecio;
+    var segundoPrecio;
+    var Apellido="Nadie"
+
+    apellido="Perez";
+    alert(apellido)
+    //alert(segundoPre); si la variable no existe no podemos hacer nada
+// Es un valor literal si esta entre ""
+	//alert("Tal cual");//Literal
+    //alert(apellido);
+    //alert("apellido")
+    //alert (); sale vacio
 }
 

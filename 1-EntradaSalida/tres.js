aa3a124d91a96f96cc3c504002bdf20b97c28896
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-07 12:24:26 -0300

    Ejercicio 9; ejercicio 3 del parcial
    sin hacer

---
+++ 1-EntradaSalida/tres.js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+
+	var precio;
+	var porcentaje;
+	var descuento;
+	var preciofinal;
+
+	prompt(precio, porcentaje);
+	preciofinal=document.getElementById('elPrecioFinal').value;
+
+
+
+}

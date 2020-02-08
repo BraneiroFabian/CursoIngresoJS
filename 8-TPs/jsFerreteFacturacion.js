89f51f581bcb9edc557684c06ed7ba26a8c02b38
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-08 19:56:42 -0300

    Tp EJ 1
    Suma, promedio y precio final + iva

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,83 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+
+	var primerprecio;
+	var segundoprecio;
+	var tercerprecio;
+	var suma;
+	var resultado;
+
+	primerprecio=document.getElementById('PrecioUno').value;
+	segundoprecio=document.getElementById('PrecioDos').value;
+	tercerprecio=document.getElementById('PrecioTres').value;
+	primerprecio=parseInt(primerprecio);
+	segundoprecio=parseInt(segundoprecio);
+	tercerprecio=parseInt(tercerprecio);
+	//el parseInt transforma en numero entero al valor entre parentesis
+
+	suma=primerprecio+segundoprecio+tercerprecio;
+	console.log(suma);
+	resultado=suma;
+	alert("El resultado es "+resultado);
+
+
 	
 }
 function Promedio () 
 {
+
+	var primerprecio;
+	var segundoprecio;
+	var tercerprecio;
+	var suma;
+	var resultado;
+	var promedio;
+
+	primerprecio=document.getElementById('PrecioUno').value;
+	segundoprecio=document.getElementById('PrecioDos').value;
+	tercerprecio=document.getElementById('PrecioTres').value;
+
+	primerprecio=parseInt(primerprecio);
+	segundoprecio=parseInt(segundoprecio);
+	tercerprecio=parseInt(tercerprecio);
+
+
+	suma=primerprecio+segundoprecio+tercerprecio;
+	console.log(suma);
+	promedio=suma/3;
+	resultado=promedio;
+
+	
+	alert("El resultado es "+resultado);
+
 	
 }
 function PrecioFinal () 
 {
+
+	var primerprecio;
+	var segundoprecio;
+	var tercerprecio;
+	var IVA;
+	var suma;
+	var resultado;
+	var PrecioFinal;
+
+
+	primerprecio=document.getElementById('PrecioUno').value;
+	segundoprecio=document.getElementById('PrecioDos').value;
+	tercerprecio=document.getElementById('PrecioTres').value;
+	primerprecio=parseInt(primerprecio);
+	segundoprecio=parseInt(segundoprecio);
+	tercerprecio=parseInt(tercerprecio);
+
+	IVA=21;
+	suma=primerprecio+segundoprecio+tercerprecio;
+	resultado=(suma*IVA)/100;
+	PrecioFinal=suma+resultado;
+	alert("El resultado es "+PrecioFinal);
+
+
 	
 }

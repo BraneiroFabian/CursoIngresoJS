c839aaffb09e96addb5b4a2e03bd2257ed921f18
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-09 01:01:59 -0300

    Ejercicio 2
    Costo pero salio, habia que dividir el resultado de perimetro*bolsascemento y bolsascal, por 4

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,81 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var cantidadAlambre;
+	var largo;
+	var ancho;
+	var perimetro;
+	var hiloAlambre;
 
-}
+	largo=document.getElementById('Largo').value;
+	ancho=document.getElementById('Ancho').value;
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+	perimetro=(largo*2+ancho*2);
+	perimetro=parseInt(perimetro);
+	hiloAlambre=parseInt(hiloAlambre);
+	hiloAlambre=3;//aca uso el 3 porque se debe hacer con 3 hilos de alambre
+
+	
+
+	console.log(perimetro);
+	cantidadAlambre=perimetro*hiloAlambre;
+	alert("Metros de alambre a comprar: "+cantidadAlambre);
+
+
+}	
+
+
 function Circulo () 
 {
+	var cantidadAlambre;
+	var radio;
+	var hiloAlambre;
+
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+
+	hiloAlambre=3;
+	hiloAlambre=parseInt(hiloAlambre);
+	diametro=radio*2;
+	console.log(diametro);
+	diametro=parseInt(diametro);
+
+	cantidadAlambre=diametro*3;
+	alert("Metros de alambre a comprar: "+cantidadAlambre);
+
 	
 }
 function Materiales () 
 {
-	
+	var largo;
+	var ancho;
+	var perimetro;
+	var bolsasCal;
+	var bolsasCemento;
+	var bolsascementoNecesitadas;
+	var bolsascalNecesitadas;
+
+	largo=document.getElementById('Largo').value;
+	ancho=document.getElementById('Ancho').value;
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+	bolsasCal=3;
+	bolsasCal=parseInt(bolsasCal);
+	bolsasCemento=2;
+	bolsasCemento=parseInt(bolsasCemento);
+
+	perimetro=parseInt(perimetro);
+	perimetro=(largo*2+ancho*2);
+
+	console.log(perimetro);
+	bolsascementoNecesitadas=perimetro*bolsasCemento/4;
+	bolsascalNecesitadas=perimetro*bolsasCal/4;
+
+	alert("Cal necesitado: "+bolsascalNecesitadas);
+	alert("Cemento necesitado: "+bolsascementoNecesitadas);
+
+
+
+
 }

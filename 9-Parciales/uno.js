b40cc61c18f0428d145dcc4ab7493ce328718814
Fabián <60743922+BraneiroFabian@users.noreply.github.com>
2020-02-13 01:12:42 -0300

    parcial 1 y 2
    asdasdasd

---
+++ 9-Parciales/uno.js
@@ -0,0 +1,17 @@
+
+function mostrar()
+{
+
+
+	var ancho;
+	var largo;
+	var	perimetro;
+
+	ancho=prompt("Ingrese ancho");
+	largo=prompt("Ingrese largo");
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+	perimetro=ancho*2+largo*2;
+
+	alert("El perímetro es: "+perimetro);
+}

dd26fc8248b868ece8274f9b2e41674a09285463
alumno <alumno>
2020-02-13 09:38:53 -0300

    Hice mi primer commit

---
+++ Curso de git.js
@@ -0,0 +1,4 @@
+git clone https://github.com/BraneiroFabian/CursoIngresoJS.git
+git add .//esto trabaja sobre el local
+git commit -m "mensaje"
+git status//te dice los cambios y modificaciones hechas

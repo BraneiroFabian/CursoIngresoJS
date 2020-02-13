30b787c8827d5262fdc0bcae89d715c7d0e2dfa2
alumno <alumno>
2020-02-13 09:31:18 -0300

    Mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,12 +1,7 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-    var primerPrecio;
-    var segundoPrecio;
-    var Apellido="Nadie"
-
-    apellido="Perez";
-    alert(apellido)
+    //testeando git
     //alert(segundoPre); si la variable no existe no podemos hacer nada
 // Es un valor literal si esta entre ""
 	//alert("Tal cual");//Literal

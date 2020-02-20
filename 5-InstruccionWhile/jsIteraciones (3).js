b49b28d5db08a07ffec19ac64a8c0cb673420713
alumno <alumno>
2020-02-20 12:03:42 -0300

    Ejercicio 4 while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-
+var clave;
 var clave = prompt("ingrese el número clave.");
+	
 
+	while(clave!="utn750")
+	{
+		clave=prompt("Le erraste");
+	}
+	alert("Clave correcta");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
+	var numero;
+	
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+
+	numero = prompt("ingrese un número entre 0 y 10.");
+	numero=parseInt(numero);
+
+	//while(!(numero>0 && numero<10))
+	while(numero<0||numero>10)
+	{
+		numero = prompt("Re,ingrese un número entre 0 y 10.pa bote");
+		numero=parseInt(numero);
+	}
 
 
+
+
+
+	
+	document.getElementById('Numero').value=numero;
 }//FIN DE LA FUNCIÓN

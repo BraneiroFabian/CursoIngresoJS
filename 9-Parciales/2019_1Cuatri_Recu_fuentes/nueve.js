cebe07906ea8b1e6ecfc54f562328e9c4d75ec42
Fabian <BraneiroFabian>
2020-02-27 19:11:15 -0300

    Ejercicio 9 parcial casi hecho, preguntar

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,42 +1,91 @@
 function mostrar()
 {
 
-	var nombreEstudiante;
-	var edad;
 	var sexo;
-	var notaFinal;
-	var respuesta;
-	var notaAprob;
-	var varonesAprob;
-	var promedio;
-	var contador;
-	var acumulador;
+	var notaIngresada;
+	var edad;
+	var respuesta=true;
+	var varonesAprobados=0;
+	var notaMenores=0;
+	var cantidadMenores=0;
+	var notaAdolescentes=0;
+	var cantidadAdolescentes=0;
+	var notaMayores=0;
+	var cantidadMayores=0;
+	var notasVarones=0;
+	var cantidadVarones=0;
+	var notaNenas=0;
+	var cantidadMujeres=0;
 
 
-	do
+	while(respuesta==true)
 	{
-	nombreEstudiante=prompt("Ingrese su nombre");
-	edad=prompt("Ingrese su edad");
-	edad=parseInt(edad);
-	sexo=prompt("Ingrese su sexo");
-	notaFinal=prompt("Ingrese su nota final");
-	notaFinal=parseInt(notaFinal);
-	}while(respuesta!="s");
-
-	while(!isNaN(nombreEstudiante))
+		do
 		{
-			nombreEstudiante=prompt("Ingrese un nombre valido");
+			edad=prompt("Ingrese su edad");
+			edad=parseInt(edad);
+
+		}while(isNaN(edad));
+
+		do
+		{
+			sexo=prompt("Ingrese el sexo");
+
+		}while(!(sexo=="f"||sexo=="m"));
+
+		do
+		{
+			notaIngresada=prompt("Ingrese la nota");
+			notaIngresada=parseInt(notaIngresada);
+		}while(isNaN(notaIngresada>0&&notaIngresada<10));
+
+		if (notaIngresada>5&&sexo=="m") 
+		{
+			varonesAprobados=varonesAprobados+1;
+		}
+		if (edad<18) 
+		{
+			notaMenores=notaMenores+notaIngresada;
+			cantidadMenores=cantidadMenores+1;
+
+		}
+		if (edad>12&&edad<18) 
+		{
+			notaAdolescentes=notaAdolescentes+notaIngresada;
+			cantidadAdolescentes=cantidadAdolescentes+1;
+		}
+		if (edad>18) 
+		{
+			notaMayores=notaMayores+notaIngresada;
+			cantidadMayores=cantidadMayores+1;
 		}
 
+		if (sexo=="m") 
+		{
+			notasVarones=notasVarones+notaIngresada;
+			cantidadVarones=cantidadVarones+1;
+
+		}
+		if (sexo=="f") 
+		{
+
+			notaNenas=notaNenas+notaIngresada;
+			cantidadMujeres=cantidadMujeres+1;
+
+		}	
 
 
 
 
+		respuesta=confirm("Presione aceptar para continuar");
+	}
 
+	document.write("Cantidad de varones aprobados: "+varonesAprobados+"<br>");
+	document.write("Promedio de notas de menores de edad: "+notaMenores/cantidadMenores+"<br>");
+	document.write("Promedio de notas de adolescentes: "+notaAdolescentes/cantidadAdolescentes+"<br>");
+	document.write("Promedio de notas de mayores: "+notaMayores/cantidadMayores+"<br>");
+	document.write("Promedio de notas varones: "+notasVarones/cantidadVarones+"<br>");
+	document.write("Promedio de notas mujeres: "+notaNenas/cantidadMujeres+"<br>");
 
-
-
-
-
-	document.write()
+	
 }
